181e779779b1e0bd2e97d4fa4d01ad4d6ed4b00d
mayoreee <mayowaegbinola@gmail.com>
2021-11-07 02:45:21 +0100

    remove hardcoded mn list

--- dapiclient/MNDiscovery/masternode_discovery.py
+++ dapiclient/MNDiscovery/masternode_discovery.py
@@ -10,19 +10,6 @@ class MasternodeDiscovery:
     def get_random_masternode(self, excluded_ips = None):
         mnlist = self.mnlist_provider.get_mn_list()
         return random.sample(mnlist, 1)[0]['service']      
-        # # Temporary fix till testnet becomes stable
-        # mnlist = [
-        #     '34.219.81.129',
-        #     '34.221.42.205',
-        #     '34.208.88.128',
-        #     '54.189.162.193',
-        #     '34.220.124.90',
-        #     '54.201.242.241',
-        #     '54.68.10.46',
-        #     '34.210.81.39',
-        #     '18.237.47.243'
-        #     ]
-        # return random.choice(mnlist)
 
     def get_mnlist(self):
         return self.mnlist_provider.get_mn_list()
